a0299729d8f766bf40373d5859a72f1eeaf3725e
Matthew Reader <mdreader@gmail.com>
2024-08-16 23:11:26 -0500

    Updated var GCP_SERVICE_ACCOUNT_KEY to FIREBASE_SERVICE_ACCOUNT

--- src/firestore/client.rs
+++ src/firestore/client.rs
@@ -7,7 +7,7 @@ use std::fs;
 pub async fn init_firestore_client() -> Result<FirestoreDb, FirestoreError> {
     dotenv().ok();
     let project_id = env::var("GCP_PROJECT_ID").expect("GCP_PROJECT_ID must be set");
-    let service_account_key = env::var("GCP_SERVICE_ACCOUNT_KEY").expect("GCP_SERVICE_ACCOUNT_KEY must be set");
+    let service_account_key = env::var("FIREBASE_SERVICE_ACCOUNT").expect("FIREBASE_SERVICE_ACCOUNT must be set");
 
     if !fs::metadata(&service_account_key).is_ok() {
         panic!("Service account key file not found: {}", service_account_key);
