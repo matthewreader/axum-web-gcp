49c523bd0731b2c5cf7f3494aecb1b2516d09831
Matthew Reader <mdreader@gmail.com>
2024-08-17 11:07:01 -0500

    Corrections to CI/CD

--- src/firestore/client.rs
+++ src/firestore/client.rs
@@ -1,20 +1,12 @@
 use firestore::{FirestoreDb, errors::FirestoreError, FirestoreDbOptions};
 use std::env;
 use dotenv::dotenv;
-use std::fs;
 
 /// Initializes and returns a Firestore database client.
 pub async fn init_firestore_client() -> Result<FirestoreDb, FirestoreError> {
     dotenv().ok();
     let project_id = env::var("GCP_PROJECT_ID").expect("GCP_PROJECT_ID must be set");
-    let service_account_key = env::var("FIREBASE_SERVICE_ACCOUNT").expect("FIREBASE_SERVICE_ACCOUNT must be set");
 
-    if !fs::metadata(&service_account_key).is_ok() {
-        panic!("Service account key file not found: {}", service_account_key);
-    }
-
-    FirestoreDb::with_options_service_account_key_file(
-        FirestoreDbOptions::new(project_id)
-            .with_database_id("orders".to_string()), service_account_key.into()
-    ).await
+    FirestoreDb::with_options(FirestoreDbOptions::new(project_id)
+            .with_database_id("orders".to_string())).await
 }
