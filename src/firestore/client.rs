29bfb535b1df658527a96063e297a643ce8d8e3b
Matthew Reader <mdreader@gmail.com>
2024-08-16 22:43:45 -0500

    Update FirestoreDb initialization to incorporate Firestore credentials

--- src/firestore/client.rs
+++ src/firestore/client.rs
@@ -1,15 +1,20 @@
 use firestore::{FirestoreDb, errors::FirestoreError, FirestoreDbOptions};
 use std::env;
 use dotenv::dotenv;
+use std::fs;
 
 /// Initializes and returns a Firestore database client.
 pub async fn init_firestore_client() -> Result<FirestoreDb, FirestoreError> {
     dotenv().ok();
     let project_id = env::var("GCP_PROJECT_ID").expect("GCP_PROJECT_ID must be set");
+    let service_account_key = env::var("GCP_SERVICE_ACCOUNT_KEY").expect("GCP_SERVICE_ACCOUNT_KEY must be set");
 
-    //FirestoreDb::new(&project_id).await
-    FirestoreDb::with_options(
+    if !fs::metadata(&service_account_key).is_ok() {
+        panic!("Service account key file not found: {}", service_account_key);
+    }
+
+    FirestoreDb::with_options_service_account_key_file(
         FirestoreDbOptions::new(project_id)
-            .with_database_id("orders".to_string())
+            .with_database_id("orders".to_string()), service_account_key.into()
     ).await
 }
